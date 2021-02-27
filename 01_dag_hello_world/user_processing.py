c560cac3e8380cde549cc7ade8afe8adf3a6e943
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-27 10:21:47 +0530

    DAG Create a Table on SQLite

--- 01_dag_hello_world/user_processing.py
+++ 01_dag_hello_world/user_processing.py
@@ -7,8 +7,8 @@ default_args = {
     'start_date': datetime(2020, 1, 1)
 }
 
-with DAG('user_processing', schedule_interval='@daily'
-        start_date=datetime(2020, 1, 1),
+with DAG('user_processing', schedule_interval='@daily',
+        default_args=default_args,
         catchup=False) as dag:
 
         #Define tasks/operators
