63530711ac90e912f7c8751bc61ef412c567dae1
Lalit R Nagpal <lalit2801@gmail.com>
2021-02-27 08:04:46 +0530

    FIRST DAG

---
+++ dag_hello_world/user_processing.py
@@ -0,0 +1,28 @@
+from airflow.models import DAG
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020, 1, 1)
+}
+
+with DAG('user_processing', schedule_interval='@daily'
+        start_date=datetime(2020, 1, 1),
+        catchup=False) as dag:
+
+        #Define tasks/operators
+        create_table = SqliteOperator(
+            task_id='create_table',
+            sqlite_conn_id='db_sqlite',
+            sql='''
+                CREATE TABLE users(
+                    email TEXT NOT NULL PRIMARY KEY,
+                    firstname TEXT NOT NULL,
+                    lastname TEXT NOT NULL,
+                    country TEXT NOT NULL,
+                    username TEXT NOT NULL,
+                    password TEXT NOT NULL
+                );
+            '''
+        )
